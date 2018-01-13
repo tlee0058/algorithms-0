1605bd38327750dae20f69d5130e45efbaff671e
tlee0058 <32435667+tlee0058@users.noreply.github.com>
2018-01-12 21:09:02 -0500

    Add files via upload

---
+++ what_do_you_knowask.js
@@ -0,0 +1,9 @@
+/*What Do You Know?
+Your function will be given an input parameter incoming. 
+Please console.log this value.*/
+        
+function wdyk(incoming){
+    console.log(incoming);
+};
+
+wdyk("hello");
