af652eb0841fe335e5f3b490bf5ed20c71dcf86c
tlee0058 <tlee0058@gmail.com>
2018-01-12 19:09:10 -0500

    add setting_and_swapping.js

---
+++ setting_and_swapping.js
@@ -0,0 +1,7 @@
+/*Set variable myNumber to 42. Set variable myName to your name. Now swap myNumber into myName & vice versa.*/
+
+var myNumber = 42;
+var myName = "Ting";
+var temp = myNumber;
+console.log (myNumber = myName);
+console.log(myName = temp);
