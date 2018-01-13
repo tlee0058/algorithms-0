c96d4930f68decc20d7506491237c2cd66ea7f1f
tlee0058 <32435667+tlee0058@users.noreply.github.com>
2018-01-12 21:17:53 -0500

    Add files via upload

---
+++ countdown_by_four.js
@@ -0,0 +1,11 @@
+/*Countdown By Fours
+Log positive numbers starting at 2016, counting down 
+by fours (exclude 0), without a FOR loop.*/
+
+var i = 2016;
+while(i > 0){
+    if(i % 4 === 0){
+        console.log(i);
+    }
+    i--;
+};
