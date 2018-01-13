248b49bc63fe77046819689958516a52ce1c7bf3
tlee0058 <32435667+tlee0058@users.noreply.github.com>
2018-01-12 19:40:25 -0500

    Add files via upload

---
+++ dont_worry_be_happy.js
@@ -0,0 +1,12 @@
+/*Don’t Worry, Be Happy
+Create beCheerful(). Within it, console.log string "good morning!" Call it 98 times.*/
+
+function beCheerful(){
+    var i = 1;
+    while(i< 99){
+        console.log("good morning!");
+        i++;
+    };
+};
+
+beCheerful();
