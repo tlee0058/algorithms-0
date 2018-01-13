900095d516f9397c7b71c805cd77afb616ebe69e
tlee0058 <32435667+tlee0058@users.noreply.github.com>
2018-01-12 20:48:06 -0500

    Add files via upload

---
+++ multiple_of_sixask.js
@@ -0,0 +1,12 @@
+/*Multiples of Six
+Print multiples of 6 up to 60,000, using a WHILE.*/
+
+var i = 0;
+while (i < 60001){
+    if(i % 6 === 0){
+        console.log(i);
+        i++;
+        }
+    
+}
+        
