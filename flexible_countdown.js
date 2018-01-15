425b367ca6c735d3ee151d90ab75c4ed108f81b1
tlee0058 <32435667+tlee0058@users.noreply.github.com>
2018-01-15 10:06:06 -0500

    Update flexible_countdown.js

--- flexible_countdown.js
+++ flexible_countdown.js
@@ -6,10 +6,11 @@ from highNum down to lowNum, using a FOR loop.
 Example: For (2,9,3), print 9 6 3 (on successive lines). */
 
 function flexible_countdown(lowNum, highNum, mult){
-    for(var i = highNum; i > lowNum; i-=mult){
+    for(var i = highNum; i > lowNum; i--){
+        if(i % mult ===0){
         console.log(i);
-    };
-}
+        };
+}};
 
 flexible_countdown(2,9,3);
 
